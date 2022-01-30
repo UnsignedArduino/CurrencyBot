c49b3d00e91e6f88283ad5f556a7c32fda90638a
UnsignedArduino <UnsignedArduino@outlook.com>
2022-01-29 20:24:09 -0500

    Can claim hourly, daily, and monthly

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import logging
 from pathlib import Path
 
+import arrow
 from dotenv import load_dotenv
-from interactions import Client, CommandContext, Embed, Member, Option, \
-    OptionType
+from interactions import Client, CommandContext, Embed, Option, OptionType
 
 from util.db import DBClient
 from util.environ import load_env_var
@@ -20,6 +20,19 @@ db_uri = load_env_var("DB_URI")
 db_name = load_env_var("DB_NAME")
 currency_name = load_env_var("CURRENCY_NAME")
 currency_name_plural = load_env_var("CURRENCY_NAME_PLURAL")
+scope = int(load_env_var("BOT_SCOPE"))
+
+CLAIM_TIMES = {
+    "hourly": 3600,
+    "daily": 86400,
+    "monthly": 2592000
+}
+
+CLAIM_VALUES = {
+    "hourly": int(load_env_var("HOURLY_CLAIM")),
+    "daily": int(load_env_var("DAILY_CLAIM")),
+    "monthly": int(load_env_var("MONTHLY_CLAIM")),
+}
 
 bot = Client(token=token)
 db = DBClient(uri=db_uri, db_name=db_name)
@@ -27,7 +40,7 @@ db = DBClient(uri=db_uri, db_name=db_name)
 
 @bot.command(name="balance",
              description="Gets how much money you or another user has!",
-             scope=702979262906368040,
+             scope=scope,
              options=[
                 Option(
                     type=OptionType.MENTIONABLE,
@@ -46,6 +59,51 @@ async def balance(ctx: CommandContext, member: str = None):
     else:
         text = f"<@{member_id}> has {user_bal} {currency_name_plural}"
     embed = Embed(description=text)
+    await ctx.send(embeds=embed)
+
+
+@bot.command(name="claim",
+             description="Claim your hourly, daily, or monthly!",
+             scope=scope,
+             options=[
+                 Option(
+                     type=OptionType.SUB_COMMAND,
+                     name="hourly",
+                     description="Claim your hourly! "
+                                 "(Which you can do...every hour)"
+                 ),
+                 Option(
+                     type=OptionType.SUB_COMMAND,
+                     name="daily",
+                     description="Claim your daily! "
+                                 "(Which you can do...every day)"
+                 ),
+                 Option(
+                     type=OptionType.SUB_COMMAND,
+                     name="monthly",
+                     description="Claim your monthly! "
+                                 "(Which you can do every 30 day period)"
+                 ),
+             ])
+async def claim(ctx: CommandContext, sub_command: str):
+    member_id = int(str(ctx.author.user.id))
+    last_claim = await db.get_last_claim(member_id=member_id,
+                                          claim_type=sub_command)
+    last_claim = last_claim.int_timestamp
+    time_diff = CLAIM_TIMES[sub_command]
+    now = arrow.now().int_timestamp
+    if now - last_claim > time_diff:
+        earned = CLAIM_VALUES[sub_command]
+        await db.change_balance(member_id=member_id, change=earned)
+        await db.set_last_claim(member_id=member_id, claim_type=sub_command,
+                                last_claim_time=now)
+        embed = Embed(description=f"You claimed your {sub_command} and got "
+                                  f"{earned} {currency_name_plural}!")
+    else:
+        time_left = arrow.get(last_claim + CLAIM_TIMES[sub_command])
+        embed = Embed(description=f"You can claim your {sub_command} in "
+                                  f"{time_left.humanize(only_distance=True)}!",
+                      color=0xFF0000)
     await ctx.send(embeds=embed)
 
 
--- src/util/db.py
+++ src/util/db.py
@@ -1,6 +1,8 @@
 import logging
 from copy import deepcopy
 
+import arrow
+from arrow.arrow import Arrow
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.util.logger import create_logger
@@ -56,6 +58,19 @@ class DBClient:
             await self.db_col.insert_one(doc)
         return doc
 
+    async def set_member(self, member_id: int, doc: dict):
+        """
+        Set a member's object.
+
+        :param member_id: The ID of the member.
+        :param doc: The member's new doc.
+        """
+        test = await self.db_col.find_one({"id": member_id})
+        if test is None:
+            await self.db_col.insert_one(doc)
+        else:
+            await self.db_col.replace_one({"id": member_id}, doc)
+
     async def get_balance(self, member_id: int) -> int:
         """
         Get a member's balance.
@@ -65,4 +80,55 @@ class DBClient:
         """
         obj = await self.get_member(member_id=member_id)
         return obj["balance"]
+
+    async def set_balance(self, member_id: int, balance: int):
+        """
+        Set a member's balance.
+
+        :param member_id: The ID of the member.
+        :param balance: The balance to set to.
+        """
+        obj = await self.get_member(member_id=member_id)
+        obj["balance"] = balance
+        await self.set_member(member_id=member_id, doc=obj)
+
+    async def change_balance(self, member_id: int, change: int):
+        """
+        Change a member's balance.
+
+        :param member_id: The ID of the member.
+        :param change: How much to change the balance by.
+        """
+        obj = await self.get_member(member_id=member_id)
+        obj["balance"] += change
+        await self.set_member(member_id=member_id, doc=obj)
+
+    async def get_last_claim(self, member_id: int,
+                             claim_type: str) -> Arrow:
+        """
+        Get the last claim time for a member.
+
+        :param member_id: The ID of the member.
+        :param claim_type: The claim type. Must be "hourly", "daily", or
+         "monthly"
+        :return: An Arrow date.
+        """
+        obj = await self.get_member(member_id=member_id)
+        return arrow.get(obj["last"][claim_type])
+
+    async def set_last_claim(self, member_id: int,
+                             claim_type: str,
+                             last_claim_time: int):
+        """
+        Set the last claim time for a member.
+
+        :param member_id: The ID of the member.
+        :param claim_type: The claim type. Must be "hourly", "daily", or
+         "monthly"
+        :param last_claim_time: Set the last claim time to this.
+        :return: An Arrow date.
+        """
+        obj = await self.get_member(member_id=member_id)
+        obj["last"][claim_type] = last_claim_time
+        await self.set_member(member_id=member_id, doc=obj)
 
