62400979df22e69806ece089e7ae9d4db65a4646
UnsignedArduino <UnsignedArduino@outlook.com>
2022-02-01 21:07:00 -0500

    Can send people currency and give special responses if the bot is a target

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ logger.debug(f"Loading token from {dotenv_path}")
 
 load_dotenv(dotenv_path=dotenv_path)
 token = load_env_var("BOT_TOKEN")
+bot_id = int(load_env_var("BOT_ID"))
 db_uri = load_env_var("DB_URI")
 db_name = load_env_var("DB_NAME")
 currency_name = load_env_var("CURRENCY_NAME")
@@ -41,6 +42,17 @@ CLAIM_VALUES = {
 }
 
 GITHUB_URL = "https://github.com/UnsignedArduino/CurrencyBot"
+
+
+def currency_naming(value: int) -> str:
+    """
+    Return the currency name plural or singular depending on the value.
+
+    :param value: The amount.
+    :return: A string which is the currency name.
+    """
+    return currency_name if value == 1 else currency_name_plural
+
 
 bot = Client(token=token)
 db = DBClient(uri=db_uri, db_name=db_name)
@@ -74,11 +86,12 @@ async def balance(ctx: CommandContext, member: str = None):
     else:
         member_id = int(member)
     user_bal = await db.get_balance(member_id)
-    if user_bal == 1:
-        text = f"<@{member_id}> has {user_bal} {currency_name}"
+    if member_id == bot_id:
+        embed = Embed(description=f"<@{member_id}> has infinite "
+                                  f"{currency_name_plural}")
     else:
-        text = f"<@{member_id}> has {user_bal} {currency_name_plural}"
-    embed = Embed(description=text)
+        embed = Embed(description=f"<@{member_id}> has {user_bal} "
+                                  f"{currency_naming(user_bal)}")
     await ctx.send(embeds=embed)
 
 
@@ -117,13 +130,60 @@ async def claim(ctx: CommandContext, sub_command: str):
         await db.change_balance(member_id=member_id, change=earned)
         await db.set_last_claim(member_id=member_id, claim_type=sub_command,
                                 last_claim_time=now)
-        embed = Embed(description=f"You claimed your {sub_command} and got "
-                                  f"{earned} {currency_name_plural}!")
+        embed = Embed(description=f'You claimed your {sub_command} and got '
+                                  f'{earned} {currency_naming(earned)}!')
     else:
         time_left = arrow.get(last_claim + CLAIM_TIMES[sub_command])
         embed = Embed(description=f"You can claim your {sub_command} in "
                                   f"{time_left.humanize(only_distance=True)}!",
                       color=0xFF0000)
+    await ctx.send(embeds=embed)
+
+
+@bot.command(name="send",
+             description="Send people coins!",
+             scope=scope,
+             options=[
+                 Option(
+                     type=OptionType.MENTIONABLE,
+                     name="member",
+                     description="The member to send coins to!",
+                     required=True
+                 ),
+                 Option(
+                     type=OptionType.INTEGER,
+                     name="amount",
+                     description="How much to send!",
+                     required=True
+                 )
+             ])
+async def send(ctx: CommandContext, member: str, amount: int):
+    from_id = int(str(ctx.author.user.id))
+    to_id = int(member)
+    from_bal = await db.get_balance(from_id)
+    if from_id == to_id:
+        embed = Embed(description=f"You can't send yourself money!",
+                      color=0xFF0000)
+    elif to_id == bot_id:
+        embed = Embed(description=f"I'm flattered! :flushed: But you can't "
+                                  f"send me money. :cry:",
+                      color=0xFF0000)
+    elif amount > from_bal:
+        embed = Embed(description=f"You do not have enough "
+                                  f"{currency_name_plural} to send!\n"
+                                  f"(You have {from_bal} "
+                                  f"{currency_naming(from_bal)} which is "
+                                  f"{amount - from_bal} less then {amount})",
+                      color=0xFF0000)
+    elif amount <= 0:
+        embed = Embed(description=f"You cannot send less then "
+                                  f"1 {currency_name}!",
+                      color=0xFF0000)
+    else:
+        await db.change_balance(member_id=from_id, change=-amount)
+        await db.change_balance(member_id=to_id, change=amount)
+        embed = Embed(description=f"Successfully sent <@{to_id}> "
+                                  f"{amount} {currency_naming(amount)}!")
     await ctx.send(embeds=embed)
 
 
